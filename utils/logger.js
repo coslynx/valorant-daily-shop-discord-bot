920e8765841eb2f28e08cf6998db9361a698775e
CosLynx AI <drixisgod@gmail.com>
2024-10-31 11:26:25 +0530

    generated file: `utils/logger.js`

---
+++ utils/logger.js
@@ -0,0 +1,29 @@
+import winston from 'winston';
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'Valorant-Daily-Shop-Bot' }),
+    timestamp(),
+    myFormat,
+  ),
+  transports: [
+    new winston.transports.Console({
+      level: 'info',
+      format: combine(
+        timestamp(),
+        myFormat,
+      ),
+    }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+export default logger;
